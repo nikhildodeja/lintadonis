b0d3cdd06e17d988d530ce0e6c280b6e568d5bac
nikhildodeja <nikhildodeja91@gmail.com>
2020-08-25 13:27:04 +0530

    first commit

---
+++ database/factory.js
@@ -0,0 +1,8 @@
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+// const Factory = use('Factory')
+
+// Factory.blueprint('App/Models/User', (faker) => {
+//   return {
+//     username: faker.username()
+//   }
+// })
